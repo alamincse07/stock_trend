2c2a28843efa1ef6f02c11ef077a7c03024629bd
Sage Rimal <srimal@campusexplorer.com>
2020-07-06 12:10:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index(): 
-    return render_template('index.html')
+    return render_template('index.html', data = ['static/' + sub for sub in  os.listdir('static')])
 
 @app.route('/',methods=['POST'])
 def get_chart(): 
@@ -17,6 +17,18 @@ def get_chart():
     get_charts(ticker)
     data = ['static/' + sub for sub in  os.listdir('static')]
     return render_template('chart.html', name = ticker, url =f'static/{ticker}.png', data=data)
+
+@app.route('/delete',methods=['POST'])
+def delete_chart(): 
+    ticker = request.form.get('ticker')
+   
+    SYM = ticker.upper()[7:-4]
+    os.remove(f'static/{SYM}.png')
+    fileList = glob.glob(f'data/{SYM}*.csv')
+    for f in fileList:
+        os.remove(f)
+    data = ['static/' + sub for sub in  os.listdir('static')]
+    return render_template('index.html', name = f'Removed {SYM}' , url =f'static/{ticker}.png', data=data)
 
 if __name__ == '__main__':
     app.run()
