e91e1d59895ca69cee446776a837fd75adcf5e25
Sage Rimal <srimal@campusexplorer.com>
2020-07-05 21:23:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index(): 
-    return render_template('index.html', name = 'AMAZON', url ='static/AMZN.png')
+    return render_template('index.html')
 
 @app.route('/',methods=['POST'])
 def get_chart(): 
